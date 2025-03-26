dcc8a1d7e38fd0eb24a8341b163e57aeb17b223b
LilianaGgon <lilianagago20@hotmail.com>
2025-03-26 16:49:43 -0600

    Inicio y fin de CRUD con SOAP funcional

---
+++ SL_WCF/Crud.svc.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace SL_WCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Crud" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Crud.svc or Crud.svc.cs at the Solution Explorer and start debugging.
+    public class Crud : ICrud
+    {
+        public void DoWork()
+        {
+        }
+
+        public SL_WCF.Result Delete(int idUsuario)
+        {
+            ML.Result result = BL.Usuario.DeleteEF(idUsuario);
+            return new SL_WCF.Result
+            {
+                Correct = result.Correct,
+                ErrorMessage = result.ErrorMessage,
+                Object = result.Object,
+                Objects = result.Objects,
+            };
+        }
+
+        public SL_WCF.Result Add(ML.Usuario usuario)
+        {
+            ML.Result result = BL.Usuario.AddEF(usuario);
+            return new SL_WCF.Result
+            {
+                Correct = result.Correct,
+                ErrorMessage = result.ErrorMessage,
+                Object = result.Object,
+                Objects = result.Objects,
+            };
+        }
+
+        public SL_WCF.Result Update(ML.Usuario usuario)
+        {
+            ML.Result result = BL.Usuario.UpdateEF(usuario);
+            return new SL_WCF.Result
+            {
+                Correct = result.Correct,
+                ErrorMessage = result.ErrorMessage,
+                Object = result.Object,
+                Objects = result.Objects,
+            };
+        }
+
+        public SL_WCF.Result GetById(int idUsuario)
+        {
+            ML.Result result = BL.Usuario.GetByIdEF(idUsuario);
+            return new SL_WCF.Result
+            {
+                Correct = result.Correct,
+                ErrorMessage = result.ErrorMessage,
+                Object = result.Object,
+                Objects = result.Objects,
+            };
+        }
+
+        public SL_WCF.Result GetAll(ML.Usuario usuario)
+        {
+            ML.Result result = BL.Usuario.GetAllEF(usuario);
+            return new SL_WCF.Result
+            {
+                Correct = result.Correct,
+                ErrorMessage = result.ErrorMessage,
+                Object = result.Object,
+                Objects = result.Objects,
+            };
+        }
+    }
+}
---
+++ SL_WCF/ICrud.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace SL_WCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ICrud" in both code and config file together.
+    [ServiceContract]
+    public interface ICrud
+    {
+        [OperationContract]
+        void DoWork();
+
+        [OperationContract]
+        SL_WCF.Result Delete(int idUsuario);
+
+        [OperationContract]
+        [ServiceKnownType(typeof(ML.Usuario))]
+        SL_WCF.Result Add(ML.Usuario usuario);
+
+        [OperationContract]
+        [ServiceKnownType(typeof(ML.Usuario))]
+        SL_WCF.Result Update(ML.Usuario usuario);
+
+        [OperationContract]
+        [ServiceKnownType(typeof(ML.Usuario))]
+        SL_WCF.Result GetById(int idUsuario);
+
+        [OperationContract]
+        [ServiceKnownType(typeof(ML.Usuario))]
+        SL_WCF.Result GetAll(ML.Usuario usuario);
+
+    }
+}
